04206bd0e797acaf541017f406075d63ecf579d6
seanwilliams <sean.williams.11@gmail.com>
2020-09-11 07:30:07 -0500

    adding testing code for tf.dataset training

--- tensorflow_developer2/13_loading_preprocessing_data/fashion_mnist_model_tfrecord.py
+++ tensorflow_developer2/13_loading_preprocessing_data/fashion_mnist_model_tfrecord.py
@@ -1,15 +1,23 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-from tensorflow.keras.layers import Flatten, Dense
+import tensorflow_datasets as tfds
+from tensorflow.keras.layers import Flatten, Dense, LayerNormalization
+from contextlib import ExitStack
+import IPython.display as display
+from sklearn.preprocessing import StandardScaler
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 import os
 
+
 """
-This class takes the fashion MNIST dataset, creates a training, validation and test set, 
-builds a sequential model and predicts the results.  
+This class takes 
+1) the fashion MNIST dataset, creates a training, validation and test set, 
+2) saves the data to TFRecord
+3) builds a sequential model and predicts the results.  
 """
 
 # Where to save the figures
@@ -17,7 +25,7 @@ PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "ann"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
 os.makedirs(IMAGES_PATH, exist_ok=True)
-
+BATCH_SIZE = 32
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=600):
     """
@@ -34,54 +42,227 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=600):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
-# Load the dataset
-(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
-# split the training set again into validation and training set
-X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full)
 
-# scale the RGB values and convert them to float
-X_valid, X_train = X_valid / 255., X_train / 255.
-X_test = X_test / 255.
+def create_proto_buffer(image, label):
+    Example = tf.train.Example
+    Features = tf.train.Features
+    Feature = tf.train.Feature
 
-plt.imshow(X_train[0], cmap="binary")
-plt.axis('off')
-save_fig("first_training_image")
-#plt.show()
+    image_string = tf.io.serialize_tensor(image)
+    #image_string = keras.backend.eval(image_string)
+    mnist_image = Example(
+        features=Features(
+            feature={
+                "image": Feature(bytes_list=tf.train.BytesList(value=[image_string.numpy()])),
+                "label": Feature(int64_list=tf.train.Int64List(value=[label]))
+            }
+        )
+    )
+    return mnist_image
+
+
+def write_tfrecords(name, dataset, n_shards=10):
+    """
+    This is from HOML example.
+    :param name: file name and location
+    :param dataset: TensorFlow dataset to split into multiple shards
+    :param n_shards: The number of shard to split the dataset into
+    :return: A list of file paths
+    """
+    # Create the file name path with list comprehensions
+    paths = ["{}.tfrecord-{:05d}-of-{:05d}".format(name, index, n_shards)
+             for index in range(n_shards)]
+    # ExitStack closes all open files at the end of the with statement
+    with ExitStack() as stack:
+        # Create a list of paths
+        writers = [stack.enter_context(tf.io.TFRecordWriter(path))
+                   for path in paths]
+        # loop through each item in the dataset and write to a file
+        for index, (image, label) in dataset.enumerate():
+            shard = index % n_shards
+            example = create_proto_buffer(image, label)
+            writers[shard].write(example.SerializeToString())
+    return paths
+
+
+# Create the feature description
+feature_descr = {
+    "image": tf.io.FixedLenFeature([], tf.string, default_value=""),
+    "label": tf.io.FixedLenFeature([], tf.int64, default_value=-1)
+}
+
+def normalize_test_data(dataset):
+    X_train = None
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_mean = scaler.mean_
+    X_std = scaler.scale_
+
+def parse_example_proto(example_proto):
+    """
+    Read the single example back.
+    :param example_proto:
+    :return: Returns a dictionary of tensors
+    """
+    # Parse the input tf.train.Example proto using the dictionary above.
+    example = tf.io.parse_single_example(example_proto, feature_descr)
+    image = tf.io.parse_tensor(example["image"], out_type=tf.uint8)
+    # image = tf.io.decode_jpeg(example["image"])
+    image = tf.reshape(image, shape=[28, 28])
+    return image, example["label"]
+
+
+def parse_example_proto_test_predict(example_proto):
+    """
+    Read the single example back for testing and prediction data.
+    :param example_proto:
+    :return: Returns a dictionary of tensors
+    """
+    # Parse the input tf.train.Example proto using the dictionary above.
+    example = tf.io.parse_single_example(example_proto, feature_descr)
+    image = tf.io.parse_tensor(example["image"], out_type=tf.uint8)
+    # image = tf.io.decode_jpeg(example["image"])
+    image = tf.reshape(image, shape=[28, 28])
+    image = tf.cast(image, tf.float32) / 255.
+    return image, example["label"]
+
+
+def dataset_reader(filepaths, repeat=1, n_readers=5, n_read_threads=None, shuffle_buffer_size=10000,
+                            n_parse_threads=5, batch_size=32, training=True):
+    """
+    Create a TensorFlow dataset object from the filepaths.
+    :param filepaths: List of file paths/patterns
+    :param repeat: repeat this dataset so each original value is seen "repeat" times
+    :param n_readers: cycle_length controls the number of input elements that are processed concurrently
+    :param n_read_threads: num_parallel_calls - makes interleave use multiple threads to fetch elements.
+    :param shuffle_buffer_size: The dataset will fill a buffer of buffer_size elements.
+    :param n_parse_threads: the number of threads to execute in parallel
+    :param batch_size: sets the size of the batch.
+    :return:
+    """
+    #dataset = tf.data.Dataset.list_files(filepaths).repeat(repeat)
+    dataset = filepaths.repeat(repeat)
+    # maps a function across this dataset and interleaves the results
+    dataset = dataset.interleave(lambda filepath: tf.data.TFRecordDataset(filepath),
+                                 cycle_length=n_readers,
+                                 num_parallel_calls=n_read_threads)
+    # Randomly shuffles the elements of this dataset, draws 1 element from buffer and replaces it with new element
+    dataset = dataset.shuffle(shuffle_buffer_size)
+    # map the preprocessing function onto the dataset.
+    if training:
+        dataset = dataset.map(parse_example_proto, num_parallel_calls=n_parse_threads)
+    else:
+        dataset = dataset.map(parse_example_proto_test_predict, num_parallel_calls=n_parse_threads)
+    # combines the dataset elements into batches
+    # prefetch fetches 1 batch of 32 records.
+    return dataset.batch(batch_size).prefetch(1)
+
+
+def create_file_paths(data_dir, file_pattern, shuffle):
+    mnist_file = os.path.join(data_dir, file_pattern)
+    return tf.data.Dataset.list_files(mnist_file, shuffle=shuffle)
+
+
+# Load the dataset, split it into training, validation and test sets and shuffle the files.
+ds_train, ds_validation, ds_test = tfds.load(name="fashion_mnist", split=["train[:50000]", "train[50000:]", "test"],
+                                             shuffle_files=True, as_supervised=True)
+
+# Shuffle the training set
+ds_train_shuffled = ds_train.shuffle(50000)
+mnist_dir = "datasets/fashion_mnist"
+mnist_file_name = "fashion_mnist.tfrecord"
+os.makedirs(mnist_dir, exist_ok=True)
+mnist_file_location = os.path.join(mnist_dir, mnist_file_name)
+
+# loop through the dataset and display the image.
+# for image_features in parsed_image_dataset:
+#   image_raw = tf.io.parse_tensor(image_features['image'], out_type=tf.uint8)
+#   image_raw2 = tf.reshape(image_raw, shape=[28, 28])
+#   #display.display(display.Image(data=image_raw))
+#   _ = plt.imshow(image_raw2, cmap="binary")
+
+# Write the data to the datasets directory
+if not os.path.exists(os.path.join(mnist_dir, "fashion_mnist_test.tfrecord.tfrecord-00000-of-00010")):
+    print(f"Writing shards of data to {mnist_dir}")
+    file_name = os.path.join(mnist_dir, "fashion_mnist_train.tfrecord")
+    write_tfrecords(file_name, ds_train)
+    file_name = os.path.join(mnist_dir, "fashion_mnist_valid.tfrecord")
+    write_tfrecords(file_name, ds_validation)
+    file_name = os.path.join(mnist_dir, "fashion_mnist_test.tfrecord")
+    write_tfrecords(file_name, ds_test)
+
+# Create the file paths
+train_filepaths = create_file_paths(mnist_dir, "*train*", shuffle=True)
+test_filepaths = create_file_paths(mnist_dir, "*test*", shuffle=True)
+valid_filepaths = create_file_paths(mnist_dir, "*valid*", shuffle=True)
+
+# Read the sharded data back
+train_dataset = dataset_reader(train_filepaths, repeat=None, shuffle_buffer_size=50000)
+valid_dataset = dataset_reader(valid_filepaths, repeat=1, training=False)
+test_dataset = dataset_reader(test_filepaths, repeat=1, training=False)
+result = test_dataset.element_spec
+result2 = result[0]
+# map the
+# image_dataset = tf.data.TFRecordDataset(mnist_file_location)
+# parsed_image_dataset = image_dataset.map(parse_example_proto)
 
 class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal",
                "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-print(X_train_full.shape)
-print(type(X_train_full))
-print(X_train.shape)
-print(type(X_train))
-model = keras.models.Sequential()
-model.add(Flatten(input_shape=[28, 28]))
-model.add(Dense(300, activation="relu"))
-model.add(Dense(100, activation="relu"))
-model.add(Dense(10, activation="softmax"))
+# Build and train the model
+model_dir = "models"
+model_name = "fashion_minst_model"
+history_name = "fashion_minst_history.npy"
+model_location = os.path.join(model_dir, model_name)
+history_location = os.path.join(model_dir, history_name)
+model = None
+history = None
+
+#for num, _ in enumerate(train_dataset):
+#    pass
+
+if not os.path.exists(model_location):
+    print("Building the model.")
+    os.makedirs(model_dir, exist_ok=True)
+    model = keras.models.Sequential()
+    model.add(LayerNormalization(input_shape=[28, 28]))
+    model.add(Flatten(input_shape=[28, 28]))
+    model.add(Dense(300, activation="relu"))
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(10, activation="softmax"))
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
+    model.build()
+    history = model.fit(train_dataset, epochs=10, validation_data=valid_dataset, steps_per_epoch=50000 // BATCH_SIZE)
+    #history = model.fit(train_dataset, epochs=10, validation_data=valid_dataset)
+    np.save(history_location, history.history)
+    model.save(model_location)
+else:
+    print("Loading the saved model.")
+    history = np.load(history_location, allow_pickle='TRUE').item() if os.path.exists(history_location) else None
+    model = tf.keras.models.load_model(model_location)
 
 print("Model Summary")
 print(model.summary())
 
-model.compile(loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
+model.evaluate(test_dataset, verbose=2)
 
-# history = model.fit(X_train_full, y_train_full, epochs=30, validation_data=(X_valid, y_valid))
-history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid))
+# one Dataset item has 32 records
+for X, y in test_dataset.take(1):
+    for i in range(5): # only loop through the first 5 records
+        plt.subplot(1, 5, i + 1)
+        plt.imshow(X[i].numpy(), cmap="binary")
+        plt.axis("off")
+        plt.title(str(y[i].numpy()) + ' ' + str(class_names[y[i].numpy()]))
+    save_fig("fashion_mnist_test.png")
 
-pd.DataFrame(history.history).plot(figsize=(8, 5))
-plt.grid(True)
-plt.ylim(bottom=0, top=1)
-save_fig("keras_learning_curves_plot")
-#plt.show()
-
-model.evaluate(X_test, y_test)
-X_new = X_test[:3]
+X_new = test_dataset.take(1)
 y_proba = model.predict(X_new)
 y_proba.round(2)
 y_pred = model.predict_classes(X_new)
+new_array = np.array(zip(y_pred, class_names[y_pred]))
 print(y_pred)
-print(np.array(class_names)[y_pred])
+print(class_names)
+#print(new_array)
 
 
 
